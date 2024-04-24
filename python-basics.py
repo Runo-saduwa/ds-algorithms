6ad30e8a7b6a4dc9cbc8f31b69900a7afde990f1
runo saduwa <orunosaduwa@gmail.com>
2024-04-24 16:19:17 +0100

    updates

---
+++ python-basics.py
@@ -0,0 +1,27 @@
+# Hello world
+print('Hello world');
+
+# Single Variables
+
+name = 'Runo'
+name = 80
+
+# Multiple variables
+
+name, age, occupation = 'Runo', 24, 'Engineer'
+
+# Conditionals
+score = 100
+if score < 50:
+  print('Below average')
+elif (score >= 50 and score <= 55):
+  print('Average')
+else: 
+  print('Above average')
+
+
+# Loops
+
+
+
+
