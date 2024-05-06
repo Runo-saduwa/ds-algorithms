c24fa10a785a86222c997f67eb3e5ba5daa9a199
runo saduwa <orunosaduwa@gmail.com>
2024-05-06 21:27:31 +0100

    updates

--- remove-duplicates.py
+++ remove-duplicates.py
@@ -80,4 +80,15 @@ def removeDuplicates(arr):
     return l
 
 
+
+def removeDuplicatesX(arr);
+   
+   l = 1
+
+   for r in range(1, len(arr)):
+      if arr[r] != arr[r-1]:
+         arr[l] = arr[r]
+         l += 1
+  
+   return l
    
